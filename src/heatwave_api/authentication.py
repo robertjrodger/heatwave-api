884a5837a8572613410e4a1d9415e3b4e792ec2c
Robert Rodger <woodenrabbit@gmail.com>
2020-05-04 20:25:04 +0200

    add api key authentication

--- src/heatwave_api/authentication.py
+++ src/heatwave_api/authentication.py
@@ -3,6 +3,7 @@ Middleware support for API-key authentication.
 """
 from typing import Sequence
 
+from fastapi import status
 from starlette.datastructures import Headers
 from starlette.responses import PlainTextResponse
 from starlette.types import ASGIApp, Receive, Scope, Send
@@ -18,9 +19,11 @@ class TrustedKeyMiddleware:
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         headers = Headers(scope=scope)
-        key = headers.get("x-api-key")
+        key = headers.get("X-API-KEY")
         if key and key in self._allowed_keys:
             await self.app(scope, receive, send)
         else:
-            response = PlainTextResponse("Invalid or missing API key.", status_code=400)
+            response = PlainTextResponse(
+                "Invalid or missing API key.", status_code=status.HTTP_403_FORBIDDEN
+            )
             await response(scope, receive, send)
