e8045faced657a494caa6d75c4eaa482d2d7a2df
Robert Rodger <woodenrabbit@gmail.com>
2020-05-10 18:42:58 +0200

    remove extra whitespace

--- src/heatwave_api/configuration.py
+++ src/heatwave_api/configuration.py
@@ -3,13 +3,11 @@ from pathlib import Path
 from starlette.config import Config
 from starlette.datastructures import CommaSeparatedStrings
 
+repo_root_dir = Path(__file__).resolve().parent.parent.parent
 config = Config()
 
 DEBUG = config("VM_API_DEBUG", cast=bool, default=False)
 KEYS = config("VM_API_KEYS", cast=CommaSeparatedStrings, default=[])
-OUTPUT_DIR = config(
-    "VM_OUTPUT_DIR",
-    cast=Path,
-    default=Path(__file__).resolve().parent.parent.parent / "output",
-)
+DATA_DIR = config("VM_DATA_DIR", cast=Path, default=repo_root_dir / "data")
+OUTPUT_DIR = config("VM_OUTPUT_DIR", cast=Path, default=repo_root_dir / "output")
 ARCHIVE_FILENAME = "archive.parquet"
